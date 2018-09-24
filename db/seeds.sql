5498a46bf41f5b2c90793ea30d41d60b9430c9cd
Chad Tao <chenchen408@gmail.com>
2018-09-24 15:43:41 -0700

    created dir and files

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name, devoured) VALUES ('Double Bacon Cheese Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Western Star Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Philly Cheese Steak Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Crispy Chicken Garden Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', false);
